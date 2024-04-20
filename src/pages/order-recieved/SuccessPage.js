ed519fdf7842cc1213379eeb210f0a6a84b2ef28
Muhammed Hamza Toptancı <toptanci.hamza@gmail.com>
2024-04-20 15:25:51 +0300

    mobile-responsive-ended

--- src/pages/order-recieved/SuccessPage.js
+++ src/pages/order-recieved/SuccessPage.js
@@ -4,37 +4,45 @@ import "./success.css";
 
 function Success({ order }) {
   return (
-    <div className="success-container">
-      <div>
-        <img src={logo} alt="" />
-      </div>
-      <div className="dividiv">
-        <p>
-          <span id="yolda">lezzetin yolda</span> <br />
-          <span id="alindi">SİPARİŞ ALINDI</span>
-        </p>
-        <hr />
-        <span id="paap">Position Absolute Acı Pizza</span>{" "}
-      </div>
-      <div className="infos">
-        <p>Boyut:{order.size}</p>
-        <p>Hamur:{order.hamur}</p>
-        <p>
-          Ek Malzemeler:{" "}
-          {Array.isArray(order.malzeme) ? order.malzeme.join(", ") : "N/A"}
-        </p>
-      </div>
-      <div className="price-card">
-        <p>Sipariş Toplamı</p>
-        <div id="secim">
-          <p>Seçimler</p>
-          <p>{order.toplamTutar - order.adet * 85.5} TL</p>
+    <div className="mainss">
+      <div className="headerss"></div>
+      <div className="success-container">
+        <div>
+          <img src={logo} alt="" />
         </div>
-        <div id="tutar">
-          <p>Seçimler</p>
-          <p>{order.toplamTutar} TL</p>
+        <div className="dividiv">
+          <p>
+            <span id="yolda">lezzetin yolda</span> <br />
+            <span id="alindi">SİPARİŞ ALINDI</span>
+          </p>
+          <hr />
+          <span id="paap">Position Absolute Acı Pizza</span>{" "}
+        </div>
+        <div className="main-info">
+          <div className="infos">
+            <p>Boyut:{order.size}</p>
+            <p>Hamur:{order.hamur}</p>
+            <p>
+              Ek Malzemeler:{" "}
+              {Array.isArray(order.malzeme) ? order.malzeme.join(", ") : "N/A"}
+            </p>
+          </div>
+        </div>
+
+        <div className="price-card">
+          <p>Sipariş Toplamı</p>
+          <div id="secim">
+            <p>Seçimler</p>
+            <p>{order.toplamTutar - order.adet * 85.5} TL</p>
+          </div>
+
+          <div id="tutar">
+            <p>Seçimler</p>
+            <p>{order.toplamTutar} TL</p>
+          </div>
         </div>
       </div>
+      <div className="footerss"></div>
     </div>
   );
 }
